ce1c7434f9c556ed15aa5db8bd5d097c2b9e7b63
Zoe <zoebelleton@gmail.com>
2020-10-03 15:29:16 +0200

    Swing addition

--- src/main/java/org/xylandia/game/ld47/SeasonLoopMain.java
+++ src/main/java/org/xylandia/game/ld47/SeasonLoopMain.java
@@ -3,18 +3,23 @@ package org.xylandia.game.ld47;
 import org.xylandia.game.ld47.utils.FileUtils;
 import org.xylandia.game.ld47.utils.InputUtils;
 
+import javax.swing.*;
 import java.util.Objects;
+
 
 
 public class SeasonLoopMain {
     public static void main(String[] args) {
 
+        JFrame gameWindow = new GameWindow("Seasons Loop");
+
         // Read the introduction file
         FileUtils.readFile("Intro");
         if (Objects.equals(InputUtils.askForInput("Please enter exactly 'Autumn' "), "Autumn")) {
-            FileUtils.readFile("Inventory", false);
+            FileUtils.readFile("Autumn");
         } else {
             System.out.println("This is not spelled correctly ...");
         }
     }
 }
+
